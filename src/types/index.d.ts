0a58c9d94ec8b3481799b31e3d1ebd40d5a8e477
leijiang <756335788@qq.com>
2021-07-15 15:32:51 +0800

    bug: typescript error

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,5 @@
 declare module 'remark-codesandbox' {
     import { Plugin } from 'unified'
     const Codesandbox: Plugin<any>
-    interface RemarkCodeboxOptions {
-        mode?: string
-    }
     export default Codesandbox
 }
